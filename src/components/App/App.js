78b603491882b2da99260db2702f54138db5b94f
Roma Kokontsev <Romeo42@ukr.net>
2021-09-05 15:47:01 +0300

    start file

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import ContactForm from '../FormContact/FormContact';
+import ContactList from '../ContactsList/ContactsList';
+import Filter from '../Filter/Filter';
+import './app.scss';
+
+export default function App() {
+  return (
+    <>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList />
+    </>
+  );
+}
---
+++ src/components/FormContact/FormContact.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import { AiOutlineUserAdd } from 'react-icons/ai';
+import s from './FormContact.module.css';
+import shortid from 'shortid';
+import { useSelector, useDispatch } from 'react-redux';
+import contactsOperations from '../../redux/operations';
+import { getContacts } from '../../redux/selectors';
+
+export default function ContactForm() {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const contactNameId = shortid.generate();
+  const contactNumberId = shortid.generate();
+
+  const dispatch = useDispatch();
+  const contacts = useSelector(getContacts);
+
+  const handleChange = e => {
+    const { name, value } = e.currentTarget;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if (name === '') {
+      alert(`Введите, пожалуйста, имя контакта.`);
+      return;
+    }
+
+    if (number === '') {
+      alert(`Введите, пожалуйста, номер телефона контакта.`);
+      return;
+    }
+
+    if (contacts.find(contact => contact.name === name)) {
+      alert(`${name} is already in contacts.`);
+      reset();
+      return;
+    }
+    dispatch(contactsOperations.addContact(name, number));
+    reset();
+  };
+
+  const reset = () => {
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <form className={s.form} onSubmit={handleSubmit}>
+      <label className={s.label} htmlFor={contactNameId}>
+        Name
+        <input
+          className={s.input}
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleChange}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+          id={contactNameId}
+        />
+      </label>
+      <label className={s.label} htmlFor={contactNumberId}>
+        Number
+        <input
+          className={s.input}
+          type="text"
+          name="number"
+          value={number}
+          onChange={handleChange}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+          id={contactNumberId}
+        />
+      </label>
+      <button className={s.button} type="submit">
+        <span className={s.btnText}>Add contact</span>
+        <AiOutlineUserAdd size="20px" color="rgb(130, 27, 112)" />
+      </button>
+    </form>
+  );
+}
