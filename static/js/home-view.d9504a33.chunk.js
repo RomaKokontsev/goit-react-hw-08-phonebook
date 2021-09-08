807137f95d04fd60ac28d877a95ddc7e1de2c8b0
Roma Kokontsev <Romeo42@ukr.net>
2021-09-08 20:08:47 +0300

    Updates

---
+++ static/js/home-view.d9504a33.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[2],{131:function(e,n,i){e.exports={wrapper:"HomeView_wrapper__31HZH",heading:"HomeView_heading__1AK0G"}},155:function(e,n,i){"use strict";i.r(n);var a=i(131),t=i.n(a),o=i(2);n.default=function(){return Object(o.jsx)("div",{className:t.a.wrapper,children:Object(o.jsx)("h1",{className:t.a.heading,children:"Hi there! Please, sign in to continue"})})}}}]);
+//# sourceMappingURL=home-view.d9504a33.chunk.js.map
